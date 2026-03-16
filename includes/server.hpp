2cd06cb083c6dba1c6d7feccfb7bfdbe58c1dca2
Luca Nicolas Terfehr <luca.terfehr@outlook.de>
2026-03-16 22:31:21 +0100

    Added a handler and the possibility to add route

--- includes/server.hpp
+++ includes/server.hpp
@@ -4,14 +4,23 @@
 #include <unordered_map>
 #include <connection.hpp>
 #include <memory>
+#include <functional>
 
 #include "socket.hpp"
+#include "http/httphandler.hpp"
+#include "http/httpmethod.hpp"
+#include "http/httpresponse.hpp"
+#include "http/httprequest.hpp"
 
 namespace SWS {
     class Server {
         private:
+            using HandlerFunc = std::function<HttpResponse(const HttpRequest&)>;
+
             SWS::Socket listening_socket;
             std::unordered_map<int, std::unique_ptr<SWS::Connection>> conns;
+
+            SWS::HttpHandler handler;
 
         public:
             /**
@@ -42,6 +51,15 @@ namespace SWS {
              * @param other The Server object to move from.
              */
             Server& operator=(const Server&& other);
+
+        public:
+            /**
+             * @brief Specifies a route with a http method and a function to execute once that route is hit!
+             * @param http_method The desired HTTP method, such as GET, POST etc...
+             * @param route The route the user has to send a request to in order to trigger that endpoint.
+             * @param func The function to execute.
+             */
+            void add_route(SWS::HttpMethod http_method, std::string route, HandlerFunc func);
     };
 }
 
