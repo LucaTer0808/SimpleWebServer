f07de84b59aa9e0dd46ab73a29fdf2416d253b81
Luca Nicolas Terfehr <luca.terfehr@outlook.de>
2026-03-27 13:42:39 +0100

    Altered IntelliSense properties and added worker thread creation

--- includes/server.hpp
+++ includes/server.hpp
@@ -4,6 +4,11 @@
 #include <unordered_map>
 #include <memory>
 #include <functional>
+#include <queue>
+#include <tuple>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
 
 #include "socket.hpp"
 #include "eventhandler.hpp"
@@ -17,12 +22,23 @@ namespace SWS {
         private:
             using HandlerFunc = std::function<HttpResponse(const HttpRequest&)>;
 
+            std::queue<std::tuple<int, std::string>> jobs;
+            std::mutex jobs_mutex;
+            std::condition_variable queue_not_empty;
+
+            std::queue<std::tuple<int, std::string>> responses;
+            std::mutex responses_mutex;
+
+            std::vector<std::jthread> worker_threads;
+
             std::unique_ptr<SWS::Socket> listening_socket; // to let it also be null
             SWS::EventHandler event_handler;
             std::unordered_map<int, std::unique_ptr<SWS::Connection>> conns;
             SWS::HttpHandler http_handler;
 
         public:
+            static constexpr size_t THREAD_MULT = 3;
+    
             /**
              * @brief Constructor for a Server object. Since all other objects are constructed via the basic constructor, we may just use the default constructor here.
              */
@@ -51,6 +67,14 @@ namespace SWS {
              */
             Server& operator=(Server&& other) = default;
 
+            /**
+             * @brief Appends an incoming job to the jobs queue to be consumed by the worker threads. A lock has to be acquired and
+             * a waiting process has to be notified.
+             * @param fd The file descriptor of the connection sending the job and also receiving the answer.
+             * @param request_string The request as a string.
+             */
+            void append_job(int fd, std::string request_string);
+
         public:
             /**
             * @brief Registers a GET-Endpoint.
@@ -62,19 +86,28 @@ namespace SWS {
             /**
              * @brief Start up the server. Before starting, all routes have to be added to ensure functionality!
              * @param port The port to listen on.
-             * @return LISTENING, if the server started properly.
-             * @return FAILURE, if the start of the webserver did not work.
+             * @param num_workers The amount of worker threads for increased concurrency. If none is passed, we multiply the value
+             * std::thread::hardware_concurrency delivers by a fixed constant. std::thread::hardware_concurrency also works as the lower bound
+             * for the amunt of threads running to at least make use of all cores. 0 by deafult.
              */
-            SWS::ServerStatus start(uint16_t port);
+            void start(uint16_t port, size_t num_workers = 0);
 
             /**
              * @brief The main loop for the master thread responsible for managing connections and distributing orders.
              */
             void master_thread_loop();
-    };
 
-    enum class ServerStatus {
-        LISTENING, FAILURE
+            /**
+             * @brief The main loop for any worker thread responsible for actually serving requests. It keeps checking if the queue is not empty
+             * and continues to consume and serve requests until it is empty. It then makes the thread wait on the condition variable until it is needed again.
+             */
+            void worker_thread_loop();
+
+            /**
+             * Calculates the required number of threads as stated in the start()-method;
+             * @param num_workers The desired number of worker threads or 0 if left basic.
+             */
+            size_t calculate_thread_number(size_t num_workers);
     };
 }
 
--- src/server.cpp
+++ src/server.cpp
@@ -12,18 +12,65 @@ void SWS::Server::get(std::string route, HandlerFunc func) {
     }
 }
 
-SWS::ServerStatus SWS::Server::start(uint16_t port) {
+void SWS::Server::start(uint16_t port, size_t num_workers = 0) {
     try {
         this->listening_socket = std::make_unique<SWS::Socket>(port);
     } catch (std::runtime_error &error) {
-        return SWS::ServerStatus::FAILURE;
+        SWS::log(SWS::LogLevel::ERROR, std::string("The SimpleWebServer could not be started!"));
+        return;
     }
-    return SWS::ServerStatus::LISTENING;
+
+    size_t actual_concurrency = this->calculate_thread_number(num_workers);
+
+    for (size_t i = 0; i < actual_concurrency; ++i) {
+        this->worker_threads.emplace_back([this]() {
+            this->worker_thread_loop();
+        });
+    }
+
+    SWS::log(SWS::LogLevel::INFO, std::string("SimpleWebServer is now listening on port ") + std::to_string(port));
+    this->master_thread_loop();
 }
 
 void SWS::Server::master_thread_loop() {
     while(true) {
-
+        std::unordered_map<int, uint32_t> events = this->event_handler.wait_events();
     }
+}
+
+// TODO: Implement
+void SWS::Server::worker_thread_loop() {
+    while(true) {}
+}
+
+void SWS::Server::append_job(int fd, std::string request_string) {
+    if (fd < 0) {
+        SWS::log(SWS::LogLevel::WARNING, "Negative file descriptor passed to append_job. Can not append the job!");
+        return;
+    }
+
+    {
+        std::lock_guard<std::mutex> lock(this->jobs_mutex);
+        this->jobs.emplace(fd, std::move(request_string));
+    }
+
+    queue_not_empty.notify_one();
+}
+
+size_t SWS::Server::calculate_thread_number(size_t num_workers) {
+    size_t actual_concurrency = 0;
+    size_t min_concurrency = std::thread::hardware_concurrency();
+
+    if (num_workers == 0) {
+        actual_concurrency = min_concurrency * SWS::Server::THREAD_MULT;
+    } else {
+        if (num_workers > min_concurrency) {
+            actual_concurrency = num_workers;
+        } else {
+            actual_concurrency = min_concurrency;
+        }
+    }
+
+    return actual_concurrency;
 }
 
